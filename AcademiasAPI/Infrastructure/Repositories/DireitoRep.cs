a9857437986bed62ac6b4e88cde3d0ed5f7715bd
pedro-h-o-soares <pedrosoares77744@gmail.com>
2025-12-11 11:40:13 -0300

    ref: remover função não implementada

--- AcademiasAPI/Infrastructure/Repositories/DireitoRep.cs
+++ AcademiasAPI/Infrastructure/Repositories/DireitoRep.cs
@@ -6,8 +6,5 @@ namespace AcademiasAPI.Infrastructure.Repositories;
 
 public class DireitoRep(AcademiaContext context) : BaseRep<Direito>(context), IDireitoRep
 {
-    public override Direito Create(Direito entity)
-    {
-        throw new NotImplementedException();
-    }
+
 }
